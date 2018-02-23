eb4a5aa305939393ed91aa2c02d222799ff2ae73
MuhammadAli16 <MuhammadAli16>
2018-02-23 16:05:47 +0000

    Added services foe business logic!

--- src/main/java/com/qa/cinema/rest/BookingEndpoint.java
+++ src/main/java/com/qa/cinema/rest/BookingEndpoint.java
@@ -1,12 +1,6 @@
 package com.qa.cinema.rest;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-import static javax.ws.rs.core.Response.Status.NOT_FOUND;
-import static javax.ws.rs.core.Response.Status.NO_CONTENT;
-
-import java.net.URI;
-import java.util.List;
-import java.util.logging.Logger;
 
 import javax.inject.Inject;
 import javax.validation.constraints.Min;
@@ -23,66 +17,37 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import com.qa.cinema.models.Booking;
-import com.qa.cinema.repositories.BookingRepository;
-import com.qa.cinema.util.JSONCreator;
+import com.qa.cinema.services.BookingService;
 
 @Path("/booking")
 public class BookingEndpoint {
 
-	private static final Logger LOGGER = Logger.getLogger(BookingEndpoint.class.getName()); 
-	
 	@Inject
-	private BookingRepository bookingRepository;
-
-	@Inject
-	private JSONCreator json;
+	private BookingService bookingService;
 
 	@GET
 	@Path("/{id : \\d+}")
 	@Produces(APPLICATION_JSON)
 	public Response getBooking(@PathParam("id") @Min(1) Integer id) {
-		LOGGER.info("in get book");
-		Booking booking = bookingRepository.find(id);
-		LOGGER.info("got book");
-		if (booking == null) {
-			LOGGER.info("no book");
-			return Response.status(NOT_FOUND).build();
-		}
-		LOGGER.info("return book");
-		return Response.ok(json.toJSON(booking)).build();
+		return bookingService.getBooking(id);
 	}
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getAllBookings() {
-
-		LOGGER.info("in get books");
-		List<Booking> bookings = bookingRepository.findAll();
-		LOGGER.info("got books");
-		if (bookings.isEmpty()) {
-			LOGGER.info("no books");
-			return Response.status(NO_CONTENT).build();
-		}
-		LOGGER.info("returning books");
-		return Response.ok(json.toJSON(bookings)).build();
+		return bookingService.getAllBookings();
 	}
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response createBooking(Booking booking, @Context UriInfo uriInfo) {
-		LOGGER.info("in make book");
-		booking = bookingRepository.create(booking);
-		LOGGER.info("book in table");
-		URI createdURI = uriInfo.getBaseUriBuilder().path(booking.getId().toString()).build();
-		LOGGER.info("find book and return");
-		return Response.created(createdURI).build();
+		return bookingService.createBooking(booking, uriInfo);
 	}
 
 	@DELETE
 	@Path("/{id : \\d+}")
 	public Response deleteBooking(@PathParam("id") @Min(1) Integer id) {
-		bookingRepository.destroy(id);
-		return Response.noContent().build();
+		return bookingService.deleteBooking(id);
 	}
 
 }
