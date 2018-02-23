f27111e2e473ef8eded595bc7af53799b362dd3b
MuhammadAli16 <MuhammadAli16>
2018-02-23 10:42:34 +0000

    Ability to modify showing entity

--- src/main/java/com/qa/cinema/rest/ShowingEndpoint.java
+++ src/main/java/com/qa/cinema/rest/ShowingEndpoint.java
@@ -4,14 +4,19 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 
 import java.net.URI;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.inject.Inject;
 import javax.validation.constraints.Min;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -20,7 +25,10 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
+import com.qa.cinema.models.Screen;
+import com.qa.cinema.models.ScreenType;
 import com.qa.cinema.models.Showing;
+import com.qa.cinema.repositories.ScreenRepository;
 import com.qa.cinema.repositories.ShowingRepository;
 import com.qa.cinema.util.JSONCreator;
 
@@ -29,6 +37,9 @@ public class ShowingEndpoint {
 
 	@Inject
 	private ShowingRepository showingRepository;
+	
+	@Inject
+	private ScreenRepository screenRepository;
 
 	@Inject
 	private JSONCreator json;
@@ -67,5 +78,35 @@ public class ShowingEndpoint {
 		showingRepository.delete(id);
 		return Response.noContent().build();
 	}
+	
+	@PUT
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response updateScreen(@FormParam("id") Integer id, @FormParam("film") String film,
+                                 @FormParam("screen") Integer screenId,
+                                 @FormParam("time") String time) {
+
+        Showing showing = showingRepository.find(id);
+        Screen screen = screenRepository.find(screenId);
+        
+        if (showing == null) {
+            Response.status(NOT_FOUND).build();
+        }
+        
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        Date date = null;
+		try {
+			date = sdf.parse(time);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+        
+        showing.setFilm(film);
+        showing.setScreen(screen);
+        showing.setTime(date);
+        
+        showingRepository.update(showing);
+    
+        return Response.noContent().build();
+    }
 
 }
